af09d9cf0af0191d9679da9862c7c1ca507bfef5
stuti <stutigoel22@gmail.com>
2024-07-30 08:25:11 +0530

    some minor changes in apiError ApiResponse

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -1,33 +1,35 @@
 import { Student } from "../models/student.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js"
+
 
 const checkRollNo =  (rollNo)=>{
     let regex = /^\d{2}[a-z]{3}\d{3}$/;
     return regex.test(rollNo);
 }
 
-const registerStudent = async(req,res)=>{
+const registerStudent =  asyncHandler(async(req,res)=>{
     const {name,rollNo,email,phoneNo} = req.body;
 
     if(!name || name.trim()===""){
-        //error throw
+        throw new ApiError(400,"Name Field is empty!!")
     }
     if(!rollNo || rollNo.trim()===""){
-        //error throw
+        throw new ApiError(400,"Roll Number Field is empty!!")
     }
     if(!email || email.trim()===""){
-        //error throw
+        throw new ApiError(400,"Email Field is empty!!")
     }
     if(!phoneNo || phoneNo.trim()===""){
-        //error throw
+        throw new ApiError(400,"Phone Number Field is empty!!")
     }
 
     if(!checkRollNo(rollNo)){
-        //error throw
+        throw new ApiError(400,"Roll Number is not in correct form")
     }
+
     
-
-
-}
+})
 
 
 export {
