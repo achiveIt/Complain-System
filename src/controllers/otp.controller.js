fa84a359b8ffecbf7d64a983cee9d4a2956600d6
stuti <stutigoel22@gmail.com>
2024-08-06 17:12:10 +0530

    OtpVerification Done

--- src/controllers/otp.controller.js
+++ src/controllers/otp.controller.js
@@ -3,7 +3,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import mailSender from '../utils/MailSender.js';
 import bcrypt from 'bcrypt'
 import ApiError from '../utils/ApiError.js'
-
+import ApiResponse from '../utils/ApiResponse.js'
 
 const sendOtpVerificationMail= asyncHandler(async(req,res)=>{
     const {email}= req.body;
@@ -43,7 +43,40 @@ const sendOtpVerificationMail= asyncHandler(async(req,res)=>{
 const verifyOtp = asyncHandler(async(req,res)=>{
     const {email,otp}= req.body
 
-    
+    const checkOtp = await Otp.findOne({
+        email
+    })
+
+    if(!checkOtp){
+        throw new ApiError(400,"Email is not yet registered!!");
+    }
+
+    const currentTime= Date.now();
+    const expiryTime= checkOtp.expiresAt;
+
+    if(expiryTime<currentTime){
+        return res.status(200).json(
+            new ApiResponse(200,{},"Otp Expired!! Regenerate Otp Verfication Mail ")
+        )
+        // throw new ApiError(400,"Otp is expired")
+    }
+
+    const hashedOtp = checkOtp.expiresAt;
+    const isCorrectOtp = await bcrypt.compare(otp,hashedOtp);
+
+    if(!isCorrectOtp){
+        throw new ApiError(400,"OTP provided is wrong!!")
+    }
+
+    //user verified
+    await Otp.deleteMany({
+        email
+    })
+
+    return res.status(200).json(
+        new ApiResponse(200,{},"User Successfully Verified")
+    )
+
 })
 
 
