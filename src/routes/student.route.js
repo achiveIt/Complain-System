b7f29d4739cfab870f474cf8e443895747ea2f12
achiveIt <Shpachori7008@gmail.com>
2024-08-09 18:02:29 +0530

    update student route

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -1,15 +1,9 @@
 import { Router } from "express";
 import { registerStudent, verifyStudentOtp } from "../controllers/student.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
-import { createComplaint } from "../controllers/complaint.controller.js";
 
 const router = Router();
 
 router.route('/register').post(registerStudent);
 router.route('/verifyMail').patch(verifyStudentOtp);
-router.route("/addComplaint").post(
-    upload.single('photo'),
-    createComplaint
-);
 
 export default router
