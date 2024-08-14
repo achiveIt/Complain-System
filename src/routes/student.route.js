cbe539849e7a08610d66ece57039fc4824150d67
stuti <stutigoel22@gmail.com>
2024-08-14 11:46:54 +0530

    update student route

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -1,15 +1,21 @@
 import { Router } from "express";
-import { changePassword, loginStudent, logoutStudent, registerStudent, updatePhoneNo, verifyStudentOtp } from "../controllers/student.controller.js";
+import { changePassword, loginStudent, logoutStudent, passwordReset, passwordResetRequest, registerStudent, updatePhoneNo, verifyStudentOtp } from "../controllers/student.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route('/register').post(registerStudent);
 router.route('/verifyMail').patch(verifyStudentOtp);
+
 router.route('/login').post(loginStudent);
 router.route('/logout').post(verifyJWT,logoutStudent);
+
 router.route('/changePassword').patch(verifyJWT,changePassword);
+
 router.route('/updatePhoneNumber').patch(verifyJWT,updatePhoneNo);
+
+router.route('/passwordResetRequest').post(verifyJWT,passwordResetRequest);
+router.route('/resetPassword').patch(verifyJWT,passwordReset);
 
 
 export default router
